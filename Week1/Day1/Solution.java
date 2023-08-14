5e2d0dd96558348e156d84f79cc820c4908d9e42
조정미 <jungim7490@gmail.com>
2023-08-14 08:52:57 +0000

    [FIX] 1일차 tab 간격 수정

--- Week1/Day1/Solution.java
+++ Week1/Day1/Solution.java
@@ -2,19 +2,19 @@
 import java.io.*;
 import java.util.StringTokenizer;
 class Main {
-	static int W, R;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		// 무게, 반복횟수 입력
-		W = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		
+    static int W, R;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        // 무게, 반복횟수 입력
+        W = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        
         // 계산
-		double RM = (double)W * (1.0 + ((double)R / 30.0));
-		
+        double RM = (double)W * (1.0 + ((double)R / 30.0));
+        
         // 출력 - 소수점 버림
-		System.out.print((int)RM);
-	}
+        System.out.print((int)RM);
+    }
 }
